27e0db3094c0bba7a123304dc60d6e9f40db6e40
Steven Frankenfield <stevenf@magenic.com>
2020-04-03 18:41:28 -0700

    starting point for express to azure function mgrtn

--- msngrwebhook/index.js
+++ msngrwebhook/index.js
@@ -1,9 +1,5 @@
-'use strict';
-
 const
-  express = require('express'),
   bodyParser = require('body-parser'),
-  app = express().use(bodyParser.json());
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
