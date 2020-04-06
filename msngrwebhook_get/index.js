89171baf0f7da588cb6eef8e8d34f2af4c64fd70
Steven Frankenfield <stevenf@magenic.com>
2020-04-06 16:52:22 -0700

    get/post webhook first try

---
+++ msngrwebhook_get/index.js
@@ -0,0 +1,34 @@
+module.exports = (context) => {
+    const {
+        req,
+        res,
+        log
+    } = context;
+
+    try {
+        // Your verify token. Should be a random string.
+        let TRUNK_TOKEN = process.env.TRUNK_TOKEN;
+
+        // Parse the query params
+        let mode = req.query['hub.mode'];
+        let token = req.query['hub.verify_token'];
+        let challenge = req.query['hub.challenge'];
+        
+        // Checks if a token and mode is in the query string of the request
+        if (mode && token) {
+        
+            // Checks the mode and token sent is correct
+            if (mode === 'subscribe' && token === TRUNK_TOKEN) {
+                
+                // Responds with the challenge token from the request
+                log('WEBHOOK_VERIFIED');
+                res.status(200).send(challenge);
+            } else {
+                // Responds with '403 Forbidden' if verify tokens do not match
+                res.status(403).send();      
+            }
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
---
+++ msngrwebhook_post/index.js
@@ -0,0 +1,25 @@
+module.exports = async function (context, req) {
+    const {
+        req,
+        res,
+        log
+    } = context;
+
+    let body = req.body;
+
+    // Checks this is an event from a page subscription
+    if (body.object === 'page') {
+
+        // Iterates over each entry - there may be multiple if batched
+        body.entry.forEach(function(entry) {
+
+        // Array will only ever contain one message.
+        let webhook_event = entry.messaging[0];
+        log(webhook_event);
+        });
+
+        res.status(200).send('EVENT_RECEIVED');
+    } else {
+        res.status(404).send();
+    }
+}
